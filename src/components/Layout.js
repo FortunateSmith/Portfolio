7cdcf94c7cd048da09975433c1a76dc4c3d8fcf2
Liam <liam.s.smith@gmail.com>
2022-10-22 22:58:51 -0400

    create layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Layout() {
+  return(
+    <div className="container">
+      <header>This is the Header</header>
+      <nav>This is the navbar</nav>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function IndexPage() {
-  return(
-    <div>
-    <h1>Liam Smith - Developer</h1>
-    <p>Under Construction</p>
-    </div>
-  )
+  return (
+    <main>
+      <Layout></Layout>
+      <h1>Liam Smith - Developer</h1>
+      <p>Under Construction</p>
+    </main>
+  );
 }
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
