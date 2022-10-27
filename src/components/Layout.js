23a892e13ac60f229f8bad6820b29b2ac8f2a702
Liam <liam.s.smith@gmail.com>
2022-10-27 16:31:14 -0400

    .

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Link } from "gatsby";
 import {
   container,
-  subContainer,
   heading,
   navLinks,
   navLinkItem,
@@ -12,39 +11,36 @@ import {
   headerCircle2,
   headerCircle3,
   headerCircleDiv,
+  subHead,
 } from "./Layout.module.css";
 
 export default function Layout({ pageTitle, children }) {
   return (
     <div className={container}>
-      <div className={subContainer}>
-        <nav>
-          {/* <StaticImage
-          alt="droplet of water on sand"
-          src="../images/aaron-burden-RgTI2KaQ5N4-unsplash.jpg"
-        /> */}
-          <ul className={navLinks}>
-            <li className={navLinkItem}>
-              <Link to="/" className={navLinkText}>
-                Home
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/about" className={navLinkText}>
-                About
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <h1 className={heading}>{pageTitle}</h1>
-          {children}
-        </div>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
       </div>
+
       <div className={headerCircleDiv}>
         <p className={headerCircle}>
           <p className={headerCircle2}>
-          <p className={headerCircle3}></p>
+            <p className={headerCircle3}></p>
           </p>
         </p>
       </div>
